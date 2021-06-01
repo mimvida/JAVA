e2c6ea68615086740e35a26fbbfb5641495e6399
Mirjam Vida <83315480+mimvida@users.noreply.github.com>
2021-06-01 22:37:23 +0200

    Eclipse repository added

---
+++ Eclipse/Edunova02/src/edunova/ZaprimanjeDokumenata.java
@@ -0,0 +1,10 @@
+package edunova;
+
+public class ZaprimanjeDokumenata {
+	public static void main(String[] args) {
+		System.out.println("Hello, it's me!");
+		System.out.println("Hello, it's me!");
+		System.out.println("Hello, it's me!");
+		System.out.println("Hello, it's me!");
+	}
+}
