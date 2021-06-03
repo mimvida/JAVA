0b06eb00b966f0592f55efba75efa171bc97c180
Mirjam Vida <83315480+mimvida@users.noreply.github.com>
2021-06-03 23:30:15 +0200

    Test #3

--- Eclipse/Edunova01/src/Github.java
+++ Eclipse/Edunova01/src/Github.java
@@ -3,5 +3,6 @@ public class Github {
     public static void main(String[] args) {
 	    System.out.println("GitHub test");
 	    System.out.println("Test #2");
+	    System.out.println("Test #3");
     }
 }
