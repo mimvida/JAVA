1fa992e784cc6150791d64a0ba5c95cef7bf1466
Mirjam Vida <83315480+mimvida@users.noreply.github.com>
2021-06-21 15:40:14 +0200

    Zadatak1 update

--- Eclipse/Edunova06/src/edunova/Zadatak1.java
+++ Eclipse/Edunova06/src/edunova/Zadatak1.java
@@ -1,9 +1,18 @@
 package edunova;
 
+import javax.swing.JOptionPane;
 
 public class Zadatak1 {
-	
-	//Program unosi 2 broja i ispisuje zbroj
+	public static void main(String[] args) {
+
+		//Program unosi 2 broja i ispisuje zbroj
+		int a,b;
+		
+		a = Integer.parseInt(JOptionPane.showInputDialog("Unesite prvi broj"));
+		b = Integer.parseInt(JOptionPane.showInputDialog("Unesite drugi broj"));
+		
+		System.out.println(a+b);
+	}
 	
 	
 }
