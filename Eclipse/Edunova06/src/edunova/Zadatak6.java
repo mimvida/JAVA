175def400e25bd0ce93eb4f3081e578fbe80782b
Mirjam Vida <83315480+mimvida@users.noreply.github.com>
2021-07-06 01:03:59 +0200

    Zadatak # 6, 7 completed

--- Eclipse/Edunova06/src/edunova/Zadatak6.java
+++ Eclipse/Edunova06/src/edunova/Zadatak6.java
@@ -3,12 +3,31 @@ package edunova;
 import java.util.Arrays;
 
 import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
+
+//import org.graalvm.compiler.core.common.spi.JavaConstantFieldProvider_OptionDescriptors;
 
 public class Zadatak6 {
-
-	// Program unosi 10 cijelih brojeva od korisnika
-	// u jednodimenzionalni niz
-	// Program ispisuje sve elemente niza
+	public static void main(String[] args) {
+		
+		// Program unosi 10 cijelih brojeva od korisnika
+		// u jednodimenzionalni niz
+		// Program ispisuje sve elemente niza
+		
+		int [] niz = new int [10];
+		niz [0] = Integer.parseInt(JOptionPane.showInputDialog("Unesi prvi cijeli broj"));
+		niz [1] = Integer.parseInt(JOptionPane.showInputDialog("Unesi drugi cijeli broj"));
+		niz [2] = Integer.parseInt(JOptionPane.showInputDialog("Unesi treci cijeli broj"));
+		niz [3] = Integer.parseInt(JOptionPane.showInputDialog("Unesi cwtvrti cijeli broj"));
+		niz [4] = Integer.parseInt(JOptionPane.showInputDialog("Unesi peti cijeli broj"));
+		niz [5] = Integer.parseInt(JOptionPane.showInputDialog("Unesi sesti cijeli broj"));
+		niz [6] = Integer.parseInt(JOptionPane.showInputDialog("Unesi sedmi cijeli broj"));
+		niz [7] = Integer.parseInt(JOptionPane.showInputDialog("Unesi osmi cijeli broj"));
+		niz [8] = Integer.parseInt(JOptionPane.showInputDialog("Unesi deveti cijeli broj"));
+		niz [9] = Integer.parseInt(JOptionPane.showInputDialog("Unesi deseti cijeli broj"));
+		
+		System.out.println(Arrays.toString(niz));
+	}
 
 	
 }
--- Eclipse/Edunova06/src/edunova/Zadatak7.java
+++ Eclipse/Edunova06/src/edunova/Zadatak7.java
@@ -1,11 +1,31 @@
 package edunova;
 
+import javax.swing.JOptionPane;
+
 public class Zadatak7 {
+	public static void main(String[] args) {
+		
+		// Prim broj je broj koji je cjelobrojno djeljiv sam s sobom
+		// i s broj 1.
+		// Za uneseni broj x = 27353 ispišite da li je prim broj	
+		
 
-	// Prim broj je broj koji je cjelobrojno djeljiv sam s sobom
-	// i s broj 1.
-	// Za uneseni broj x = 27353 ispišite da li je prim broj
+//		int x=27353;
+		int x;
+		int brojac=0;
+		x=Integer.parseInt(JOptionPane.showInputDialog("Unesi broj koji zelis provjeriti"));
+		for (int i=2;i<=x;i++) {
+			if(x%i==0) {
+				brojac++;
+				System.out.println(brojac);
+			}
+		}
+		if (brojac<=1) {
+			System.out.println("Broj je prime.");
+		}else {
+			System.out.println("Broj nije prime");
+		}
 
-	
-
-}
+			//		if (x%2==0||x%3==0)
+	}    
+}   
